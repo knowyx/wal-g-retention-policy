b8424ad89745c180bfde6b3aced4a4b3c29edfba
knowyx <knowyx@gmail.com>
2026-02-02 17:55:57 +0300

    fixed names to pass golangci-linit, renamed values in json and struct, removed comments with old code, made better logging about errors

--- cmd/pg/retention-policy/wal-g-policy.go
+++ cmd/pg/retention-policy/wal-g-policy.go
@@ -9,13 +9,13 @@ import (
 	"strings"
 )
 
-type retention_part struct {
-	Name          string
-	Default_value int
-	Description   string
+type retentionPart struct {
+	Name        string
+	Value       int
+	Description string
 }
 
-func get_paths() map[string]string {
+func getPaths() map[string]string {
 	output := make(map[string]string)
 	for i := 1; i < len(os.Args); i++ {
 		if strings.Split(os.Args[i], "=")[0] == "--walg-path" {
@@ -26,37 +26,41 @@ func get_paths() map[string]string {
 			}
 		}
 	}
-	_, exist_walg := output["wal-g"]
-	_, exist_config := output["config-path"]
-	if exist_walg == false {
+	_, existWalg := output["wal-g"]
+	_, existConfig := output["config-path"]
+	if !existWalg {
 		output["wal-g"] = "wal-g"
 	}
-	if exist_config == false {
+	if !existConfig {
 		output["config-path"] = "./walg_policy.json"
 	}
 	return output
 }
 
-func read_retention_settings(path string) []retention_part {
-	data_str, err := os.ReadFile(path)
+func readRetentionSettings(path string) []retentionPart {
+	dataStr, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatalf("Error while opening settings file: %s", err)
+		log.Fatalf("Error while opening settings file at %s. Error: %s", path, err)
 	}
-	var data_formated []retention_part
-	json.Unmarshal(data_str, &data_formated)
-	return data_formated
+	var dataFormated []retentionPart
+	err = json.Unmarshal(dataStr, &dataFormated)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal data from json %s. Error: %s", path, err)
+	}
+	return dataFormated
 }
 
-func update_data_with_arguments(data []retention_part) []retention_part {
+func updateDataWithArguments(data []retentionPart) []retentionPart {
 	for i := 1; i < len(os.Args); i++ {
 		for j := 0; j < len(data); j++ {
 			if strings.Split(os.Args[i], "=")[0] == "--"+data[j].Name {
 				value := strings.Split(os.Args[i], "=")[1]
-				int_val, err := strconv.Atoi(value)
+				intVal, err := strconv.Atoi(value)
 				if err != nil {
-					log.Fatal(err)
+					log.Fatalf("Wrong data at the left side of string %s <-- here. Should be int, not %T."+
+						" Error: %s", value, value, err)
 				}
-				data[j].Default_value = int_val
+				data[j].Value = intVal
 			}
 		}
 	}
@@ -64,46 +68,16 @@ func update_data_with_arguments(data []retention_part) []retention_part {
 }
 
 func main() {
-	//read file
-	//file, err := os.Open(os.Args[2])
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	// defer file.Close()
-
-	// data := make([]byte, 64)
-
-	// var fileData string
-	// for {
-	// 	_, err := file.Read(data)
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	// fileData += string(data[:n])
-	// }
 	logFile, err := os.OpenFile("wal-g-policy.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatal("Failed to open log file:", err)
+		log.Fatalf("Failed to open log file: %s", err)
 	}
 	defer logFile.Close()
 	log.SetOutput(logFile)
 	fmt.Print("Welcome to wal-g-policy utility! It will help to to observe copyies of your db with policies\n")
-	paths := get_paths()
-	file_data := read_retention_settings(paths["config-path"])
-	retention_data := update_data_with_arguments(file_data)
+	paths := getPaths()
+	fileData := readRetentionSettings(paths["config-path"])
+	retentionData := updateDataWithArguments(fileData)
 
-	fmt.Printf("\nUsing this polycies: %v\n", retention_data)
-
-	// // if json -> work with json unmarshal
-	// arg1 := os.Args[1]
-	// if arg1 == "json=1" {
-
-	// } else {
-	// 	//else work with text
-	// 	os.Stdout.Write(data)
-	// }
-
-	// //print args
-	// fmt.Println(os.Args)
+	fmt.Printf("\nUsing this polycies: %v\n", retentionData)
 }
